0f639b41bd3d86d57f86b69e0e53328cb9bddb3a
Zoe Sadler <ZoeS@burton.com>
2019-06-26 15:25:19 -0400

    Updates

---
+++ precache-manifest.a42fc1bc1e864d9dcb3ba16db8bf0ea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d480baad6d56c9987346244e419dd9",
+    "url": "/react-redux-beacon-gtag/index.html"
+  },
+  {
+    "revision": "1731ced5167b7dd8011e",
+    "url": "/react-redux-beacon-gtag/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-redux-beacon-gtag/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1731ced5167b7dd8011e",
+    "url": "/react-redux-beacon-gtag/static/js/main.c1490deb.chunk.js"
+  },
+  {
+    "revision": "d34c639939e911ab99ef",
+    "url": "/react-redux-beacon-gtag/static/js/runtime~main.1d701ffa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-redux-beacon-gtag/static/media/logo.5d5d9eef.svg"
+  }
+]);
